aaf3bc744494b870a9b3e4919be9061d695f2b2f
Cristobal1234566 <97963511+Cristobal1234566@users.noreply.github.com>
2022-01-31 21:00:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('El pepe ÙwÚ')
+
+let variable = 'soy una variable UwU'
+
+const constante = 3.1416
+
+let bool = true 
+
+let persona = {
+    nombre: 'cristobal,',
+    edad: 10,
+    mayorDeEdad: false
+}
